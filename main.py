9768461fcc71c43a0ff04bf5d19abc4cadb927be
ludusdigital <ludusdigital@gmail.com>
2024-03-18 11:52:26 +0100

    update

--- main.py
+++ main.py
@@ -4,74 +4,76 @@ from llama_index.llms.openai import OpenAI
 import openai
 from llama_index.core import SimpleDirectoryReader
 import os
+import shutil
+import docx2txt 
 
-st.set_page_config(page_title="RAG chatbot",
-                   page_icon="🤖",
-                   layout="centered",
-                   initial_sidebar_state="auto",
-                   menu_items=None)
+st.set_page_config(page_title="RAG chatbot", page_icon="🤖", layout="centered")
 
-# Ovde unosimo OpenAI API ključ da bi ChatGPT model mogao da radi
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 st.title("Razgovor sa mojim znanjem 🧠")
 
-if "messages" not in st.session_state.keys(
-):  # Initialize the chat messages history
-  st.session_state.messages = [{
-      "role":
-      "assistant",
-      "content":
-      "Baza vašeg znanja je učitana i spreman sam za pitanja 🧠🚀"
-  }]
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
 
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-  with st.spinner(
-      text="Učitavam podatke iz baze! Ovo može da potraje 1-2 minuta."):
-    reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-    docs = reader.load_data()
-    service_context = ServiceContext.from_defaults(llm=OpenAI(
-        model="gpt-3.5-turbo-1106",
-        temperature=0.5,
-        system_prompt=
-        "Ti si ekspert za davanje preciznih i efikasnih odgovora vezano za moju bazu znanja. Jezik: svaki odgovor isključivo piši na srpskom jeziku, koristeći srpsku gramatiku."
-    ))
-    index = VectorStoreIndex.from_documents(docs,
-                                            service_context=service_context)
-    return index
+if not os.path.exists('data'):
+    os.makedirs('data')
 
+uploaded_file = st.file_uploader("Uploaduj .txt, .pdf ili .docx fajl", type=['txt', 'pdf', 'docx'])
+if uploaded_file is not None:
+    if uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        # Ako je fajl .docx, koristite docx2txt za ekstrakciju teksta
+        text = docx2txt.process(uploaded_file)
+        file_path = os.path.join('data', uploaded_file.name.replace('.docx', '.txt'))
+        with open(file_path, "w") as text_file:
+            text_file.write(text)
+        st.success(f"'{uploaded_file.name}' je uspešno sačuvan kao tekst.")
+    else:
+        # Obrada za .txt i .pdf ostaje ista
+        file_path = os.path.join('data', uploaded_file.name)
+        with open(file_path, "wb") as f:
+            shutil.copyfileobj(uploaded_file, f)
+        st.success(f"'{uploaded_file.name}' je uspešno sačuvan.")
+    
+    def load_data():
+        with st.spinner(text="Učitavam podatke iz baze! Ovo može da potraje 1-2 minuta."):
+            reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+            docs = reader.load_data()
+            service_context = ServiceContext.from_defaults(llm=OpenAI(
+                model="gpt-3.5-turbo-1106",
+                temperature=0.5,
+                system_prompt="Ti si ekspert za davanje preciznih i efikasnih odgovora vezano za moju bazu znanja. Jezik: svaki odgovor isključivo piši na srpskom jeziku, koristeći srpsku gramatiku."
+            ))
+            index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+            return index
+    
+    index = load_data()
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+    st.session_state.data_loaded = True  # Označava da su podaci učitani
+    # Postavljanje poruke o uspešnom učitavanju podataka
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "Baza vašeg znanja je učitana i spreman sam za pitanja 🧠🚀"}]
 
-index = load_data()
+if prompt := st.chat_input("Tvoje pitanje"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
-  st.session_state.chat_engine = index.as_chat_engine(
-      chat_mode="condense_question", verbose=True)
-
-if prompt := st.chat_input(
-    "Tvoje pitanje"):  # Prompt for user input and save to chat history
-  st.session_state.messages.append({"role": "user", "content": prompt})
-
-# Display the prior chat messages with custom avatars
 for message in st.session_state.messages:
-  role = message["role"]
-  content = message["content"]
-  if role == "user":
-    with st.chat_message(role, avatar="🤔"):  # Change to desired user avatar
-      st.write(content)
-  elif role == "assistant":
-    with st.chat_message(role,
-                         avatar="🧠"):  # Change to desired assistant avatar
-      st.write(content)
+    role = message["role"]
+    content = message["content"]
+    if role == "user":
+        with st.chat_message(role, avatar="🤔"):
+            st.write(content)
+    elif role == "assistant":
+        with st.chat_message(role, avatar="🧠"):
+            st.write(content)
 
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-  with st.chat_message("assistant",
-                       avatar="🧠"):  # Assistant's response with custom avatar
-    with st.spinner("Razmišljam..."):
-      response = st.session_state.chat_engine.chat(prompt)
-      st.write(response.response)
-      message = {"role": "assistant", "content": response.response}
-      st.session_state.messages.append(
-          message)  # Add response to message history
+# Pre nego što pokušate da pristupite poslednjoj poruci, proverite da li lista sadrži neke poruke
+if len(st.session_state.messages) > 0 and st.session_state.messages[-1]["role"] != "assistant" and "data_loaded" in st.session_state:
+    with st.chat_message("assistant", avatar="🧠"):
+        with st.spinner("Razmišljam..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
+
